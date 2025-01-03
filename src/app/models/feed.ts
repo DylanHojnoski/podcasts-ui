44861c1b4fd96e1ae987d869c255337ca0194052
Dylan Hojnoski <dylanhoj@gmail.com>
2025-01-02 20:21:31 -0500

    Added categories on feeds

--- src/app/models/feed.ts
+++ src/app/models/feed.ts
@@ -1,3 +1,4 @@
+import { Category } from "./category";
 import { Post } from "./post";
 
 export class Feed {
@@ -6,4 +7,5 @@ export class Feed {
   image?: string;
   url?: string;
   posts: Post[] = [];
+  categories: Category[] = [];
 }
